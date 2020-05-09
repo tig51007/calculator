f8eca0ce5b70c9368d8186a65984f4cc4e6ef8c5
Hongtaek Ju <hongtaek.ju@gmail.com>
2020-05-09 22:49:36 +0900

    add jacoco test

--- src/main/java/com/juht/calculator/Calculator.java
+++ src/main/java/com/juht/calculator/Calculator.java
@@ -6,4 +6,7 @@ public class Calculator {
 	int sum(int a, int b) {
 		return a + b;
 	}
+	int sub(int a, int b) {
+		return a - b;
+	}
 }
--- src/main/java/com/juht/calculator/CalculatorController.java
+++ src/main/java/com/juht/calculator/CalculatorController.java
@@ -14,4 +14,10 @@ class CalculatorController {
 			   @RequestParam("b") Integer b) {
 				return String.valueOf(calculator.sum(a, b));
 		}
+
+	@RequestMapping("/sub")
+		String sub(@RequestParam("a") Integer a,
+			   @RequestParam("b") Integer b) {
+				return String.valueOf(calculator.sub(a, b));
+		}
 }
