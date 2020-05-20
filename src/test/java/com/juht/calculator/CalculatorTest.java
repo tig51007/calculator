69f7fb514e0d5f394e703d24f647b93703beded1
ABC <xyz@abc.com>
2020-05-20 17:32:10 +0900

    Add javadoc comment to CalculatorTest.java

--- src/test/java/com/juht/calculator/CalculatorTest.java
+++ src/test/java/com/juht/calculator/CalculatorTest.java
@@ -2,6 +2,10 @@ package com.juht.calculator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
+/**
+  * This is JavaDoc comment
+  */
+
 public class CalculatorTest {
 	private Calculator calculator = new Calculator();
 
