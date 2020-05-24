a8c9d46f64cf764f47ccb595027372701a3c9095
ABC <xyz@abc.com>
2020-05-24 14:39:00 +0900

    Add javadoc comment to CalculatorTest.java

---
+++ src/test/java/com/tig51007/calculator/CalculatorTest.java
@@ -0,0 +1,21 @@
+package com.tig51007.calculator;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+  * This is JavaDoc comment
+  */
+
+public class CalculatorTest {
+	private Calculator calculator = new Calculator();
+
+	@Test
+	public void sum() {
+		assertEquals(5, calculator.sum(2, 3));
+	}
+
+	@Test
+	public void sub() {
+		assertEquals(-1, calculator.sub(2, 3));
+	}
+}
